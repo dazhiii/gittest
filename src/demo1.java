e84ad5781e0d19d1c26f8b428cb0382215683ef7
dazhiii <25407497229@qq.com>
2022-07-08 11:32:46 +0800

    使用mix提交

--- src/demo1.java
+++ src/demo1.java
@@ -1,10 +1,8 @@
-import java.sql.SQLOutput;
-
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("git test......");
         System.out.println("第二次修改");
         System.out.println("第三次修改");
-        System.out.println("第四次修改");
+        System.out.println("这次使用mixed方法");
     }
 }
